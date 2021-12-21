478f4115e7e953f5ca0a6fab6fa0d89a9e0c57f0
Kartikey Tewari <kartikeya30@gmail.com>
2021-12-21 16:46:47 +0530

    add solutions for 2021 day-5

---
+++ 2021/5/a.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<pair<pair<int,int>, pair<int,int>>> arr(n);
+
+    int x_max=0;
+    int y_max=0;
+    for (int i=0;i<=n-1;i++)
+    {
+        cin >> arr[i].first.first >> arr[i].first.second;
+        cin >> arr[i].second.first >> arr[i].second.second;
+
+        x_max=max(x_max, arr[i].first.first);
+        x_max=max(x_max, arr[i].second.first);
+        y_max=max(y_max, arr[i].first.second);
+        y_max=max(y_max, arr[i].second.second);
+    }
+
+    vector<vector<int>> board(x_max+1, vector<int>(y_max+1, 0));
+    for (int i=0;i<=n-1;i++)
+    {
+        cout << "i= " << i << endl;
+        if (arr[i].first.first==arr[i].second.first)
+        {
+            int alpha=min(arr[i].first.second, arr[i].second.second);
+            int beta=max(arr[i].first.second, arr[i].second.second);
+            for (int j=alpha;j<=beta;j++)
+            {
+                board[arr[i].first.first][j]+=1;
+            }
+        }
+
+        if (arr[i].first.second==arr[i].second.second)
+        {
+            int alpha=min(arr[i].first.first, arr[i].second.first);
+            int beta=max(arr[i].first.first, arr[i].second.first);
+            for (int j=alpha;j<=beta;j++)
+            {
+                board[j][arr[i].first.second]+=1;
+            }
+        }
+    }
+
+    for (int i=0;i<=x_max;i++)
+    {
+        for (int j=0;j<=y_max;j++)
+        {
+            cout << board[j][i] << " ";
+        }
+        cout << endl;
+    }
+
+
+    cout << "done" << endl;
+    int count=0;
+    for (int i=0;i<=x_max;i++)
+    {
+        for (int j=0;j<=y_max;j++)
+        {
+            if (board[i][j]>=2)
+            {
+                count++;
+            }
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
